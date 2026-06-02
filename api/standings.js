e712af445e031f68b39b47c4c1c7df0894eadba2
ArielsitoY2K <ariel.video@gmail.com>
2026-06-02 14:31:34 -0300

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -2,8 +2,7 @@ const https = require('https');
 
 module.exports = async function handler(req, res) {
     const API_KEY = process.env.SPORTS_API_KEY;
-    // Si la tabla de posiciones se obtiene con /groups, cambia 'standings' por 'groups' aquí abajo
-    const url = 'https://v2.football.sportsapipro.com/api/world-cup-2026/standings';
+    const url = 'https://v2.football.sportsapipro.com/api/world-cup-2026/fixtures';
 
     const options = {
         headers: {
